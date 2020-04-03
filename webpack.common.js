f64f5e65f41d6ed9125a4229cfd2068884e59db3
Brian Andress <brainthinks@protonmail.com>
2020-04-03 12:42:15 -0400

    simplify and consolidate webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -8,8 +8,6 @@ const WriteFilePlugin = require('write-file-webpack-plugin');
 
 const packageJson = require('./package.json');
 
-const DESTINATION_PATH = path.resolve(__dirname, 'dist');
-
 function generateProgressBarPlugin (name) {
   const building = chalk.bold(`Building ${name} page...`);
   const bar = chalk.bgBlue.white.bold('[:bar]');
@@ -22,153 +20,87 @@ function generateProgressBarPlugin (name) {
   });
 }
 
-function generateExtractSassPlugin () {
+function generateConfig (name, entry) {
   // @see - https://github.com/webpack-contrib/sass-loader
-  return new ExtractTextPlugin({
+  const extractSass = new ExtractTextPlugin({
     filename: '[name].css',
   });
-}
 
-function generateWriteFilePlugin () {
-  return new WriteFilePlugin();
-}
-
-function generateJsRule (srcPath) {
   return {
-    test: /\.js$/,
-    loader: 'babel-loader?cacheDirectory=true',
-    options: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            exclude: [
-              '@babel/plugin-transform-typeof-symbol',
+    name,
+    entry: {
+      // @see - https://github.com/webpack-contrib/webpack-serve/issues/27
+      [name]: [
+        entry,
+      ],
+    },
+    output: {
+      path: path.resolve(
+        __dirname,
+        'dist'
+      ),
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader?cacheDirectory=true',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  exclude: [
+                    '@babel/plugin-transform-typeof-symbol',
+                  ],
+                },
+              ],
+            ],
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import',
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-class-properties',
             ],
           },
-        ],
-      ],
-      plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-class-properties',
-      ],
-    },
-    // @see - https://github.com/webpack/webpack/issues/2031
-    include: [
-      srcPath,
-      // @see - https://github.com/visionmedia/debug/issues/668
-      path.resolve(
-        __dirname,
-        'node_modules',
-        'debug'
-      ),
-    ],
-  };
-}
-
-function generateStyleRules (extractSass) {
-  return [
-    // @todo - postcss?
-    // @see - https://github.com/bensmithett/webpack-css-example/blob/master/webpack.config.js
-    {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'url-loader',
-    },
-    {
-      test: /\.s?css$/,
-      use: extractSass.extract({
-        fallback: 'style-loader',
-        use: [
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      }),
-    },
-  ];
-}
-
-function generateClspConfig () {
-  const name = packageJson.name;
-  const srcPath = path.resolve(
-    __dirname,
-    'src',
-    'js'
-  );
-
-  const extractSass = generateExtractSassPlugin();
-
-  return {
-    name,
-    entry: {
-      // @see - https://github.com/webpack-contrib/webpack-serve/issues/27
-      [name]: [
-        path.resolve(
-          srcPath,
-          'video-js-plugin',
-          'index.js'
-        ),
-      ],
-      iovPlayer: [
-        path.resolve(
-          srcPath,
-          'iov',
-          'index.js'
-        ),
-      ],
-    },
-    output: {
-      path: DESTINATION_PATH,
-      filename: '[name].js',
-    },
-    module: {
-      rules: [
-        generateJsRule(srcPath),
-        ...generateStyleRules(extractSass),
-      ],
-      // noParse: /video.js/
-    },
-    externals: {
-      'video.js': 'videojs',
-    },
-    plugins: [
-      generateProgressBarPlugin(name),
-      extractSass,
-      generateWriteFilePlugin(),
-    ],
-  };
-}
-
-function generateAdvancedDemoConfig () {
-  const name = 'advanced';
-  const srcPath = path.resolve(
-    __dirname,
-    'demo',
-    'advanced'
-  );
-
-  const extractSass = generateExtractSassPlugin();
-
-  return {
-    name,
-    entry: {
-      // @see - https://github.com/webpack-contrib/webpack-serve/issues/27
-      [name]: [
-        path.resolve(srcPath, 'main.js'),
-      ],
-    },
-    output: {
-      filename: '[name].js',
-      path: DESTINATION_PATH,
-    },
-    module: {
-      rules: [
-        generateJsRule(srcPath),
-        ...generateStyleRules(extractSass),
+          // @see - https://github.com/webpack/webpack/issues/2031
+          include: [
+            path.resolve(
+              __dirname,
+              'src'
+            ),
+            path.resolve(
+              __dirname,
+              'demo'
+            ),
+            // @see - https://github.com/visionmedia/debug/issues/668
+            path.resolve(
+              __dirname,
+              'node_modules',
+              'debug'
+            ),
+          ],
+        },
+        // @todo - postcss?
+        // @see - https://github.com/bensmithett/webpack-css-example/blob/master/webpack.config.js
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          loader: 'url-loader',
+        },
+        {
+          test: /\.s?css$/,
+          use: extractSass.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+              },
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          }),
+        },
       ],
     },
     resolve: {
@@ -179,14 +111,30 @@ function generateAdvancedDemoConfig () {
     plugins: [
       generateProgressBarPlugin(name),
       extractSass,
-      generateWriteFilePlugin(),
+      new WriteFilePlugin(),
     ],
   };
 }
 
 module.exports = function () {
   return [
-    generateClspConfig(),
-    generateAdvancedDemoConfig(),
+    generateConfig(
+      packageJson.name,
+      path.resolve(
+        __dirname,
+        'src',
+        'js',
+        'index.js'
+      )
+    ),
+    generateConfig(
+      'demo-advanced',
+      path.resolve(
+        __dirname,
+        'demo',
+        'advanced',
+        'index.js'
+      )
+    ),
   ];
 };
