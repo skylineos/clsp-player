8ec8f82bd84cf98ccb043a7ce865540f60fa324c
Brian Andress <bandress@skylinenet.net>
2020-04-03 12:44:23 -0400

    advanced demo doesn't rely on pre-built clsp-player, fix demo pages, clear dist folder on serve

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -2,9 +2,9 @@
 
 source ./scripts/utils.sh
 
-# dps "Building..."
-# yarn run build
-# ec "Successfully built!" "Failed to build!"
+dps "Building..."
+yarn run build
+ec "Successfully built!" "Failed to build!"
 
 dps "Bringing up webpack dev server..."
 ./scripts/serve.js
