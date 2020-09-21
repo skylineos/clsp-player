c1b523b334a5c0167cd59cd164faaf0207b14e38
Brian Andress <bandress@skylinenet.net>
2020-09-21 08:52:14 -0400

    added some jsdocs

--- test/jest/utils.js
+++ test/jest/utils.js
@@ -4,6 +4,10 @@ const {
   v4: uuidv4,
 } = require('uuid');
 
+/**
+ * Generate a universally-unique string.  Useful for comparisons of arguments
+ * etc. in tests.
+ */
 function generateUniqueString () {
   return uuidv4().toString();
 }
